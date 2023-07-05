62412302128f90b4792b10db1f8c6193f8cc2f3d
Kishore <Kishoresm.sm@gmail.com>
2023-07-05 13:28:15 +0530

    first

---
+++ ReactAspCRUDBackend/Models/Student.cs
@@ -0,0 +1,16 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace ReactAspCRUDBackend.Models
+{
+    public class Student
+    {
+        [Key]
+        public int id { get; set; }
+        public string stname { get; set; }
+        public string course
+        {
+            get; set;
+        }
+
+    }
+}
---
+++ ReactAspCRUDBackend/Models/StudentDbContext.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.EntityFrameworkCore;
+using ReactAspCRUDBackend.Models;
+
+namespace ReactAspCrudBackend.Models
+{
+    public class StudentDbContext : DbContext
+    {
+        public StudentDbContext(DbContextOptions<StudentDbContext> options) : base(options)
+        {
+        }
+        public DbSet<Student> Student { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer("Data Source=quickcartdb02.database.windows.net; Initial Catalog=quickcartDB; User Id=capstone; password=Kishore@123; TrustServerCertificate= True");
+        }
+    }
+}
