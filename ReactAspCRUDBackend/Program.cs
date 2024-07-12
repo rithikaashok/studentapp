e536668b9bad730fd2b188cc7683a144e5bddc85
rithikaashok <145738865+rithikaashok@users.noreply.github.com>
2024-07-12 12:56:57 +0530

    Update Program.cs

--- ReactAspCRUDBackend/Program.cs
+++ ReactAspCRUDBackend/Program.cs
@@ -1,10 +1,9 @@
 using Microsoft.EntityFrameworkCore;
-using ReactAspCrudBackend.Models;
+using ReactAspCRUDBackend.Models; // Ensure this matches your project namespace
 
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
-
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
@@ -25,6 +24,7 @@ if (app.Environment.IsDevelopment())
     app.UseSwagger();
     app.UseSwaggerUI();
 }
+
 app.UseHttpsRedirection();
 app.UseAuthorization();
 app.MapControllers();
